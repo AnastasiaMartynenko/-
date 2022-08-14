dee79cf6bca8f333c2fc149cf6fc99373ac2b79e
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-15 03:18:58 +0400

    the first task

--- Homeworks/Homework2/Program.cs
+++ Homeworks/Homework2/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int MiddleDigit (int number)
 {
    int dec = number / 10 % 10;
    return dec;
 }
-int randomNumber = new Random().Next(100,1000);
+int randomNumber = new Random().Next(100, 999);
 int newNumber = MiddleDigit(randomNumber);
-
+*/
 
 //Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 //645 -> 5
