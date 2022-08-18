bbdf10cbfc26a47e85970d305ff5826879faa16d
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-19 00:54:54 +0400

    final version of this seminar

--- Seminars/Seminar4/Program.cs
+++ Seminars/Seminar4/Program.cs
@@ -53,25 +53,25 @@ int user_number = Convert.ToInt32(Console.ReadLine());
 int composion = FindFactorial(user_number);
 Console.WriteLine($"Factorail of {user_number} is {composion}");
 */
-//Задача 4. Напишите программу, которая вывод массив из восьми элементов, заполненный нолями и единицами в случайном порядке.
-
+//Задача 4, которую не хватило ума переписать. Она неправильная. Напишите программу, которая вывод массив из восьми элементов, заполненный нолями и единицами в случайном порядке.
+/*
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
   int[] newArray = new int[size];
 
-  for(int i = 0; i <= size; i++)
-  {
+  for(int i = 0; i < size; i++)
+  
     newArray[i] = new Random().Next(minValue, maxValue + 1); //maxValue + 1 - потому, что максимально(последнее значение всегда на один меньше, т.к. индексация начинается с нуля)
-  }
+  
   return newArray; //массив возвращается без скобок, как обычная переменная
 }
 
 void ShowArray(int[] array)
 {
   for(int i = 0; i < array.Length; i++)
- {
+ 
     Console.Write($"array[i] + "); 
-  }
+  
   Console.WriteLine(); // Здесь мы используем эту конструкцию, чтобы возникла пустая строка и все вместе не слипалось
   
 }
@@ -85,4 +85,27 @@ Console.Write("Imput max value for an element: ");
 int user_max = Convert.ToInt32(Console.ReadLine());
 
 int[] myArray = CreateRandomArray(user_array, user_min, user_max);
+ShowArray(myArray);
+*/
+// Задача 4. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+int [] newArray = new int[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray (a, min, max);
 ShowArray(myArray);
