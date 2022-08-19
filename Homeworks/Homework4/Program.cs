690812b34b5f4da2d134a7ff47f18f9f7801d839
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-19 19:12:31 +0400

    added task number 27

---
+++ Homeworks/Homework4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+/*
+int FindSum (int number)
+{
+   int sum = 0;
+   for(int current = 0; current <= number; current++)
+   sum = current + sum;
+   return sum;
+}
+Console.Write("Input an intenger number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+int sum_of_digits = FindSum(user_number);
+Console.WriteLine($"Sum of all digit from {user_number} is {sum_of_digits}");
+*/
+
