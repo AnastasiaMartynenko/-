994572a1e2211891a59b527e98fef30cc07717a5
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-20 02:15:37 +0400

    added the last task

--- Homeworks/Homework4/Program.cs
+++ Homeworks/Homework4/Program.cs
@@ -10,7 +10,7 @@ int FindNumberInDegree (int a, int b)
        result = result * a;
     }
     
-    return result;
+    return result;d
 }
 Console.Write("Input an intenger number: ");
 int user_number = Convert.ToInt32(Console.ReadLine());
@@ -18,7 +18,7 @@ Console.Write("Input a degree in which you want to raise the number : ");
 int user_degree = Convert.ToInt32(Console.ReadLine());
 
 int final_result = FindNumberInDegree(user_number, user_degree);
-Console.WriteLine($"{user_number} in degree of {user_degree} equals {final_result}");
+Console.WriteLine($"{user_number} in {user_degree} degree equals {final_result}");
 */
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -43,18 +43,31 @@ Console.WriteLine($"Sum of all digit from {user_number} is {sum_of_digits}");
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 //6, 1, 33 -> [6, 1, 33]
 
-void DandomArray8(int[] array)
-
-Console.WriteLine("Input an intenger number");
-int a = Convert.ToInt32(Console.ReadLine());
-
-
-
+int[] CreateRandomArray(int size) 
 {
-    int [] newArray = new int[size];
-
-
-
-
+int[] newArray = new int[8]; // в начале нам нужно выделить под массив память, т.е. создать ему переменную массива - int[] newArra
+                                 // new int[8] - это мы создали новую переменную, в которой есть память типа int для восьми ячеек массива (восемь - size)
+{
+    for (int index = 0; index < size; index++)
+    {
+        Console.Write($"Input any element of array: ");
+        newArray[index] = Convert.ToInt32(Console.ReadLine());//то, что введет пользователь мы положим в массив под index-ом
+    }
+    
+    return newArray; //массив возвращаем без скобок
+} 
 
 }
+void PrintArray(int[] array) //этот новый метод нужен для того, чтобы вывести на экран уже сгенерированный ранее массив.
+{
+    for(int index = 0; index < 8; index++) 
+        Console.Write(array[index] + " "); // - этo нужно для того, чтобы вывести на экран все элементы массива через пробел
+        Console.WriteLine(); // это нужно, чтобы напечатать пустую строку
+}
+
+
+Console.WriteLine("Input size for array you want to see: ");
+int user_array_size = Convert.ToInt32(Console.ReadLine());
+int[]our_last_array = CreateRandomArray(user_array_size); //создали новую переменню и положили туда метод
+
+PrintArray(our_last_array); //возвращем void метод, с методом в скобках
