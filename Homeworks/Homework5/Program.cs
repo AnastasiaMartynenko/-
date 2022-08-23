061e383818dabe7007948268c2b4b85ddf6554e6
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-23 23:17:10 +0400

    final version

--- Homeworks/Homework5/Program.cs
+++ Homeworks/Homework5/Program.cs
@@ -1,70 +1,4 @@
-﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-//[3, 7, 23, 12] -> 19
-//[-4, -6, 89, 6] -> 0
-
-int[] CreateRandomArray(int size, int minValue, int maxValue)
-{
-int [] newArray = new int[size];
-for (int i = 0; i < size; i++)
-    newArray[i] = new Random().Next(minValue, maxValue + 1);
-    return newArray;
-}
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    Console.WriteLine();
-}
-
-//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-//[3 7 22 2 78] -> 76
-/*
-int[] CreateRandomArray(int size, int minValue, int maxValue)
-{
-int [] newArray = new int[size];
-for (int i = 0; i < size; i++)
-    newArray[i] = new Random().Next(minValue, maxValue + 1);
-    return newArray;
-}
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    Console.WriteLine();
-}
-
-
-int FindDifBetweenMaxMin(int[] array)
-{   
-    
-    int max_element = 0;
-    int min_element = 0;
-    int result;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if(array[i] > max_element) max_element = array[i];
-        if(array[i] < min_element) min_element = array[i];
-        
-    }
-    result =  max_element - min_element;
-    return result;
-    
-}   
-
-Console.Write("Input size for array: ");
-int array_size = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value of element: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value of element: ");
-int max = Convert.ToInt32(Console.ReadLine());
-
-
-int[] myArray = CreateRandomArray (array_size, min, max);
-ShowArray(myArray);
-int dif = FindDifBetweenMaxMin(myArray);
-Console.WriteLine($"The difference between min and max elements is {dif}");
-*/
-//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 /*
 int[] CreateRandomArray(int size)
@@ -81,15 +15,13 @@ void ShowArray(int[] array)
     Console.WriteLine();
 }
 int ShowCountEvenNumbers(int[] array)
-{   
-    int count = 0;
-    int even = 0;
+{   int count = 0;
     for (int i = 0; i < array.Length; i++)
-       if(array[i]%2 == 0)
-        {
-          count = even++;
-        }
-        return count;
+    {
+        if(array[i] % 2 == 0) 
+        count++;  
+    }
+    return count;
 }
 Console.Write("Input size for array: ");
 int array_size = Convert.ToInt32(Console.ReadLine());
@@ -104,3 +36,88 @@ int count_even_numbers = ShowCountEvenNumbers(myArray);
 Console.WriteLine($"The count of all even elements of array is {count_even_numbers}");
 */
 
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+int [] newArray = new int[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+int FindSumOddNumbers(int[] array)
+{
+  int sum = 0;
+  
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 > 0 || i % 2 < 0) 
+        sum = sum + array[i];
+    }
+    return sum;
+}
+Console.Write("Input size for array: ");
+int array_size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(array_size, min, max);
+ShowArray(myArray);
+int sum_odd_numbers = FindSumOddNumbers(myArray);
+Console.WriteLine($"Sum of all odd index elements of array is {sum_odd_numbers}");
+*/
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+int[] CreateRandomArray(int size, int minValue, int maxValue) //когда я меняла int на double программа не срабатывала
+{
+int [] newArray = new int[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+int FindDifBetweenMaxMin(int[] array)
+{   
+    
+    int max_element = array[0];
+    int min_element = array[0];
+    int result;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max_element) max_element = array[i];
+        if(array[i] < min_element) min_element = array[i];
+        
+    }
+    result =  max_element - min_element;
+    return result;
+    
+}   
+Console.Write("Input size for array: ");
+int array_size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray (array_size, min, max);
+ShowArray(myArray);
+int dif = FindDifBetweenMaxMin(myArray);
+Console.WriteLine($"The difference between min and max elements is {dif}");
---
+++ Seminars/Seminar6/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины
+//a < b + c
+/*
+bool CheckTriangle(int a, int b, int c)
+{
+    bool result = true;
+    if(a < b + c || b < a + c || c < a + b)
+    {
+        return result;
+    }
+    else return false;
+}
+Console.WriteLine($"Input first side: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Input second side: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Input third side: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (CheckTriangle(a,b,c)) Console.WriteLine ("It is a triangle");
+else Console.WriteLine ("It is not a triangle");
+*/
+//Задача 2.Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+/*
+void Fibonachchi (int n, int a, int b)
+{
+    Console.Write($"{a} {b} ");
+    int fib = a + b;
+    int fib2 = b;
+    int fib3 = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        Console.Write($"{fib} ");
+        fib3 = fib + fib2;
+        fib2 = fib;
+        fib = fib3;
+    }
+
+}
+Console.Write("Введи сколько цифр надо: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи первое число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи второе число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Fibonachchi(n, a, b);
+
+*/
