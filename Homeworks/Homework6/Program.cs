d9f08776b74a705ce47924e05f1a685b2179a4b8
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-27 01:36:32 +0400

    added thr second task

--- Homeworks/Homework6/Program.cs
+++ Homeworks/Homework6/Program.cs
@@ -38,6 +38,34 @@ int positive_numbers = FindPositiveNumbers(myArray);
 Console.WriteLine($"There are {positive_numbers} positive numbers");
 */
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
-//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+//Сначала надо сократить эти формулы y = k1 * x + b1, y = k2 * x + b2;
+// y = k1 * x + b1, y = k2 * x + b2 
+// k1 * x + b1 = k2 * x + b2
+//(k1 - k2) * x = b2 - b1
+//x = (b2 - b1) / (k1 - k2)
+//y = k1 * x + b1
+
+void FindCrossingPoint (double b1, double k1, double b2, double k2) // это координаты, которые мы будем просить пользователя ввести
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = (k2 * b1 - k1 * b2) / ( k2 - k1);
+    if(k1 ==k2)
+    Console.WriteLine($"Lines do not intersect");
+    else
+    Console.WriteLine($"The crossing points are: {x} and {y}");
+}
+Console.WriteLine("Input b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+FindCrossingPoint(k1, b1, k2, b2);
 
