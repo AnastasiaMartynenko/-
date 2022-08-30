575be320ce1084200d2b7d6f2795c11cad4e0fcc
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-30 20:12:07 +0400

    crap

--- Homeworks/Homework7/Program.cs
+++ Homeworks/Homework7/Program.cs
@@ -135,7 +135,7 @@ void Show2dArray(int[,] array)
         Console.WriteLine();
     }
 }         
-void FindColumnsAVG (int[,] array)
+void FindColumnsAVG (int[,] array) 
 {
    int count = array.GetLength(1);
    int sum = 0;
@@ -165,6 +165,53 @@ int[,] myArray = CreateRandom2dArray(m, n, min, max);
 Show2dArray(myArray);
 FindColumnsAVG(myArray);
 */
-  
+//Второй способ решения, но тоже не получается ничего
 
+  int[,] CreateRandom2dArray(int rows, int columns,int minValue, int maxValue)
+{ 
+   int[,] newArray = new int[rows, columns];
+   for(int i = 0; i < rows; i++) 
+   {
+      for(int j = 0; j < columns; j++)
+      {
+        newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+      }
+   }
+   return newArray;
+}  
+void Show2dArray(int[,] array) 
+{
+    for(int i = 0; i < array.GetLength(0); i++) 
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write(array[i, j] + " "); 
+        }
+        Console.WriteLine();
+    }
+}   
+void FindColumnAVG ([]int array)
+{
+   int count = 1;
+   int sum = 0;
+   for (int current = 0; current < count; current++)
+   {
+      sum = sum + current;
+      int avg = sum / count;
+      count++;
+   }
+   Console.WriteLine($"Average of numbers in that column is {sum / count}");
+}
 
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max posiible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+FindColumnAVG(myArray);
