31ffb62a6ca7e3bdff868a067a25bee43ff76702
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-09-04 02:05:08 +0400

    added the last task

--- Homeworks/Homework9/Program.cs
+++ Homeworks/Homework9/Program.cs
@@ -18,3 +18,17 @@ int FindSum(int a, int b)
 }
 Console.WriteLine(FindSum(2, 7));
 */
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29
+*/
+/*
+int Ackerman(int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if(m > 0 && n == 0) return Ackerman(m - 1, 1);
+    else if(m > 0 && n > 0) return Ackerman(m - 1, Ackerman(m, n - 1));
+    return Ackerman(m, n);
+}
+Console.WriteLine(Ackerman(2, 3));
+*/
