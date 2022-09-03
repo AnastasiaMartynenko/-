379571a1c460d2413abb272a7ebb939df94c5724
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-09-04 01:31:08 +0400

    added the first task

---
+++ Homeworks/Homework9/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+/*
+int sum = 0;
+int FindSum(int a, int b)
+{
+    if(a <= b)
+    {
+        sum = a + sum;
+        a++;
+        FindSum(a, b);
+    }
+    return sum;
+    
+}
+Console.WriteLine(FindSum(2, 7));
+*/
