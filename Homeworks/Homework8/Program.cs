03d522a2597650571a72cd354219643509cdc779
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-09-04 00:08:07 +0400

    god knows I tried

--- Homeworks/Homework8/Program.cs
+++ Homeworks/Homework8/Program.cs
@@ -157,13 +157,13 @@ void ShowFirst2dArray(int[,] array)
 {
     {
     for(int i = 0; i < array.GetLength(0); i++)
-    {
+        {
         for(int j = 0; j < array.GetLength(1); j++)
             Console.Write(array[i,j] + " ");
         
         Console.WriteLine();
+        }
     }
-}
 }
 
 int[,] CreateSecondRandom2dArray(int rows, int columns, int minValue, int maxValue)
@@ -184,7 +184,6 @@ void ShowSecond2dArray(int[,] array)
         Console.WriteLine();
     }
 }
-
 int[,] FindCompositionOfMatrixes(int[,] matrix1, int[,] matrix2)
 {
     int[,] composition  = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
@@ -239,3 +238,61 @@ Console.WriteLine();
 int[,] matrixComposition = FindCompositionOfMatrixes(myFirstArray, mySecondArray);
 ShowComposotion(matrixComposition);
 */
+/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+int[,,] CreateRandom3DArray(int array1, int array2, int array3, int mint, int max) 
+{
+    int[,,] result = new int[array1, array2, array3];
+
+    for (int i = 0; i < array1; i++)
+    {
+        for (int j = 0; j < array2; j++)
+        {
+            for (int k = 0; k < array3; k++)
+            {
+                result[i, j, k] = new Random().Next(10, 100);
+            }
+
+        }
+    }
+
+    return result;
+}
+void Show3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Input size for the first array: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input size for the second array: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input size for the third array: ");
+int o = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[, ,] myArray = CreateRandom3DArray(m, n, o, min, max);
+Show3DArray(myArray);
+
+
